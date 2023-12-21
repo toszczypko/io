536666bc72c68a61247e2c322e7609188f70dc5d
HelloImSophie <121552277+HelloImSophie@users.noreply.github.com>
2023-12-21 16:28:10 +0100

    Add files via upload

---
+++ Sophie's playtime in papa's pizzeria/budu.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <string>
+
+// Product class
+class Pizza {
+public:
+	void setDough(const std::string& dough)
+	{
+		this->dough = dough;
+	}
+	void setSauce(const std::string& sauce)
+	{
+		this->sauce = sauce;
+	}
+	void setTopping(const std::string& topping)
+	{
+		this->topping = topping;
+	}
+	void setBottoming(const std::string& bottoming)
+	{
+		this->bottoming = bottoming;
+	}
+	void displayPizza() const
+	{
+		std::cout << "\nPizza with Dough: " << dough
+			<< "\nSauce: " << sauce
+			<< "\nTopping: " << topping;
+		if (!bottoming.empty())	std::cout << "\nBottoming: " << bottoming;
+		std::cout << "\n\n";
+	}
+private:
+	std::string dough;
+	std::string sauce;
+	std::string topping;
+	std::string bottoming;
+};
+// Abstract builder class
+class PizzaBuilder {
+public:
+	virtual void buildDough() = 0;
+	virtual void buildSauce() = 0;
+	virtual void buildTopping() = 0;
+	virtual void buildBottoming()
+	{
+		pizza.setBottoming("brak");
+	}
+	virtual Pizza getPizza() const = 0;
+private:
+	Pizza pizza;
+};
+// Concrete builder for a specific type of pizza
+class HawaiianPizzaBuilder : public PizzaBuilder {
+public:
+	void buildDough() override
+	{
+		pizza.setDough("Pan Dough");
+	}
+	void buildSauce() override
+	{
+		pizza.setSauce("Hawaiian Sauce");
+	}
+	void buildTopping() override
+	{
+		pizza.setTopping("Ham and Pineapple");
+	}
+	Pizza getPizza() const override { return pizza; }
+private:
+	Pizza pizza;
+};
+// Concrete builder for another my type of pizza :3
+class KacperOlakPizzaBuilder : public PizzaBuilder {
+public:
+	void buildDough() override
+	{
+		pizza.setDough("ciasto od babci");
+	}
+	void buildSauce() override
+	{
+		pizza.setSauce("ketchup and passata potion");
+	}
+	void buildTopping() override
+	{
+		pizza.setTopping("pieczarki z pieczarkami");
+	}
+	void buildBottoming() override
+	{
+		pizza.setBottoming("pieniazek na szczescie");
+	}
+	Pizza getPizza() const override { return pizza; }
+private:
+	Pizza pizza;
+};
+// Concrete builder for another type of pizza
+class SpicyPizzaBuilder : public PizzaBuilder {
+public:
+	void buildDough() override
+	{
+		pizza.setDough("Thin Dough");
+	}
+	void buildSauce() override
+	{
+		pizza.setSauce("Spicy Tomato Sauce");
+	}
+	void buildTopping() override
+	{
+		pizza.setTopping("Pepperoni and Jalapenos");
+	}
+	Pizza getPizza() const override { return pizza; }
+private:
+	Pizza pizza;
+};
+// Director class that orchestrates the construction
+class Cook {
+public:
+	void makePizza(PizzaBuilder& builder)
+	{
+		builder.buildDough();
+		builder.buildSauce();
+		builder.buildTopping();
+		builder.buildBottoming();
+	}
+};
+int main()
+{
+	Cook cook;
+	HawaiianPizzaBuilder hawaiianBuilder;
+	cook.makePizza(hawaiianBuilder);
+	Pizza hawaiianPizza = hawaiianBuilder.getPizza();
+	hawaiianPizza.displayPizza();
+	SpicyPizzaBuilder spicyBuilder;
+	cook.makePizza(spicyBuilder);
+	Pizza spicyPizza = spicyBuilder.getPizza();
+	spicyPizza.displayPizza();
+
+	KacperOlakPizzaBuilder cookslittlehelp;
+	cook.makePizza(cookslittlehelp);
+	Pizza Mypizza = cookslittlehelp.getPizza();
+	Mypizza.displayPizza();
+
+	return 0;
+}
---
+++ Sophie's playtime in papa's pizzeria/budu2.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <string>
+// Product class
+class Computer {
+public:
+	void setCPU(const std::string& cpu) {
+		cpu_ = cpu;
+	}
+	void setMemory(const std::string& memory) {
+		memory_ = memory;
+	}
+	void setStorage(const std::string& storage) {
+		storage_ = storage;
+	}
+	void display() {
+		std::cout << "CPU: " << cpu_ << std::endl;
+		std::cout << "Memory: " << memory_ << std::endl;
+		std::cout << "Storage: " << storage_ << std::endl;
+	}
+private:
+	std::string cpu_;
+	std::string memory_;
+	std::string storage_;
+};
+// Builder interface
+class ComputerBuilder {
+public:
+	virtual void buildCPU(const std::string& cpu) = 0;
+	virtual void buildMemory(const std::string& memory) = 0;
+	virtual void buildStorage(const std::string& storage) = 0;
+	virtual Computer getResult() = 0;
+};
+// Concrete Builder
+class DesktopComputerBuilder : public ComputerBuilder {
+public:
+	DesktopComputerBuilder() {
+		computer_ = Computer();
+	}
+	void buildCPU(const std::string& cpu) override {
+		computer_.setCPU(cpu);
+	}
+	void buildMemory(const std::string& memory) override {
+		computer_.setMemory(memory);
+	}
+	void buildStorage(const std::string& storage) override {
+		computer_.setStorage(storage);
+	}
+	Computer getResult() override {
+		return computer_;
+	}
+private:
+	Computer computer_;
+};
+// Director
+class ComputerAssembler {
+public:
+	Computer assembleComputer(ComputerBuilder& builder) {
+		builder.buildCPU("Intel i7");
+		builder.buildMemory("16GB");
+		builder.buildStorage("512GB SSD");
+		return builder.getResult();
+	}
+};
+class ComputerAssemblerAlt {
+public:
+	Computer assembleComputer(ComputerBuilder& builder) {
+		builder.buildCPU("Intel i5");
+		builder.buildMemory("8GB");
+		builder.buildStorage("512GiB SSD");
+		return builder.getResult();
+	}
+};
+
+int main() {
+	DesktopComputerBuilder desktopBuilder;
+	ComputerAssembler assembler;
+	Computer desktop = assembler.assembleComputer(desktopBuilder);
+	std::cout << "Desktop Computer Configuration:" << std::endl;
+	desktop.display();
+
+	ComputerAssemblerAlt assembler2;
+	Computer desktop2 = assembler2.assembleComputer(desktopBuilder);
+	std::cout << "Desktop Computer Configuration:" << std::endl;
+	desktop2.display();
+	return 0;
+}
